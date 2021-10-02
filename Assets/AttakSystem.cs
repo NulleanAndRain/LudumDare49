7b318595a45cb94ca026b20c2958a1137a1a3c2b
ShinnikovVA <mr.shchinnikov@mail.ru>
2021-10-02 23:54:40 +0400

    no created

---
+++ Assets/AttakSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttakSystem : MonoBehaviour
+{
+    public int hp = 100;
+    [Header("Damage")]
+    public int attakDamage;
+    public int bodyDamaje;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TouchDamage()
+    {
+
+    }
+
+    public void MakeAnAttak()
+    {
+
+    }
+}
