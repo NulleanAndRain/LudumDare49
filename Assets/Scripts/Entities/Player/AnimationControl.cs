57adce94000f5249a6618246cb4162873d0c6d07
Nullean <mr000lean@gmail.com>
2021-10-03 14:03:45 +0400

    added GetCurrentAnimLength method

--- Assets/Scripts/Entities/Player/AnimationControl.cs
+++ Assets/Scripts/Entities/Player/AnimationControl.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class AnimationControl : MonoBehaviour
 {
     [SerializeField] private Animator _animator = null;
+    private int _layer = 0;
 
     public void TriggerAnimation(AnimationTrigger trigger)
     {
         if (trigger == AnimationTrigger.None) return;
         _animator.SetTrigger(trigger.ToString());
     }
+
+    public float GetCurrentAnimLength() => _animator.GetCurrentAnimatorStateInfo(_layer).length;
 }
 public enum AnimationTrigger
 {
